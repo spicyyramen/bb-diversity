97773fc024829225f58469c069d2a94d2ec3ef10
spicyyramen <88313330+spicyyramen@users.noreply.github.com>
2021-12-12 22:47:16 -0800

    bubble chart

--- static/js/charts.js
+++ static/js/charts.js
@@ -91,5 +91,33 @@ function buildCharts(sample) {
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout);
+
+    // create bubble chart
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [
+      {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: 'Electric'
+        }
+      }
+    ];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      hovermode: 'closest'
+      
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+
   });
 }
